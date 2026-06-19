2f4c51d1326d0773b1ba225d03362f8eeb785ee6
Rafael-Boscolo <rbf.boscolo@gmail.com>
2026-06-19 14:47:00 -0300

    add method get and put

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { TaskDto } from './task.Dto';
 
 @Injectable()
@@ -9,5 +9,27 @@ export class TaskService {
     create(task: TaskDto){
         this.tasks.push(task);
         console.log(this.tasks)
+    }
+
+    findById(id: string): TaskDto {
+        const foundTaks = this.tasks.filter(t => t.id === id);
+
+        if(foundTaks.length) {
+            return foundTaks[0];
+        }
+
+        throw new HttpException(`Task with id ${id} not found`, HttpStatus.NOT_FOUND);
+    }
+
+    update(task: TaskDto) {
+        let taskIndex = this.tasks.findIndex(t => t.id === task.id);
+
+        if(taskIndex >= 0){
+            this.tasks[taskIndex] = task;
+            console.log(this.tasks);
+            return;
+        }
+
+        throw new HttpException(`Task with id ${task.id} not found`, HttpStatus.BAD_REQUEST);
     }
 }
