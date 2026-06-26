8ae98a8f427156afa1b9db296edb4218e62b91c2
Rafael-Boscolo <rbf.boscolo@gmail.com>
2026-06-26 17:13:30 -0300

    add db typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,14 @@ import { UsersModule } from './users/users.module';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), TaskModule, UsersModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({ 
+      
+  }),
+    ConfigModule.forRoot({ isGlobal: true }), TaskModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, AuthService],
 })
