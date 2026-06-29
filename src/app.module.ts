7035f3deb7a70e6971556db2731fd4d6d3aa0de2
Rafael-Boscolo <rbf.boscolo@gmail.com>
2026-06-29 13:04:28 -0300

    create folder db and entities, migraditons and configuration db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { UsersModule } from './users/users.module';
-import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { DbModule } from './db/db.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({ 
-      
-  }),
-    ConfigModule.forRoot({ isGlobal: true }), TaskModule, UsersModule, AuthModule],
+    ConfigModule.forRoot({ isGlobal: true }), 
+    TaskModule, UsersModule, AuthModule, DbModule],
   controllers: [AppController],
-  providers: [AppService, AuthService],
+  providers: [AppService],
 })
 export class AppModule {}
